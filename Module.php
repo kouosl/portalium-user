1d5a34fe7238ac13aad371b8019d8bf07efaf5c3
sh4deofsun <by.yalcin.batuhan@gmail.com>
2019-04-26 13:48:46 +0300

    deleting duplicates

--- Module.php
+++ Module.php
@@ -9,7 +9,8 @@ use yii\filters\auth\HttpBearerAuth;
 use yii\filters\auth\QueryParamAuth;
 use yii\web\HttpException;
 
-class Module extends \kouosl\base\Module{
+class Module extends \kouosl\base\Module
+{
     public $controllerNamespace = '';
 
     public function init(){
@@ -17,41 +18,8 @@ class Module extends \kouosl\base\Module{
         $this->registerTranslations();
     }
 
-    public function behaviors(){
-        $behaviors = parent::behaviors();
-        switch ($this->namespace) {
-            case 'backend': {
-
-            };
-                break;
-            case 'frontend': {
-
-            };
-                break;
-            case 'api': {
-                $behaviors['authenticator'] = [
-                    'class' => CompositeAuth::className(),
-                    'authMethods' => [
-                        HttpBasicAuth::className(),
-                        HttpBearerAuth::className(),
-                        QueryParamAuth::className(),
-                    ],
-                ];
-            };
-                break;
-            case 'console': {
-
-            };
-                break;
-            default: {
-                throw new HttpException(500, 'behaviors' . $this->namespace);
-            };
-                break;
-        }
-        return $behaviors;
-    }
-
-    public function registerTranslations(){
+    public function registerTranslations()
+    {
         Yii::$app->i18n->translations['user/*'] = [
             'class' => 'yii\i18n\PhpMessageSource',
             'sourceLanguage' => 'en-US',
@@ -62,11 +30,13 @@ class Module extends \kouosl\base\Module{
         ];
     }
 
-    public static function t($category, $message, $params = [], $language = null){
+    public static function t($category, $message, $params = [], $language = null)
+    {
         return Yii::t('user/' . $category, $message, $params, $language);
     }
 
-    public static function initRules(){
+    public static function initRules()
+    {
         return $rules = [
             [
                 'class' => 'yii\rest\UrlRule',
